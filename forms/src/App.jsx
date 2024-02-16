ab21cce06f89632a5fb4e455e796e792877699b8
auriliu <149513433+auriliu@users.noreply.github.com>
2024-02-16 02:15:52 +0100

    forms. shaky.

---
+++ forms/src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+
+function App() {
+  return (
+    <form className="form">
+      <div className="form-group error">
+        <label className="label" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="input"
+          type="email"
+          id="email"
+          value="test@test.com"
+        />
+        <div className="msg">Must end in @webdevsimplified.com</div>
+      </div>
+      <div className="form-group">
+        <label className="label" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="input"
+          value="Password123!"
+          type="password"
+          id="password"
+        />
+      </div>
+      <button className="btn" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default App;
--- user_list/src/main.jsx
+++ user_list/src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 // import App from "./App.jsx";
 
 // import { Todo } from "./Todo.jsx";
-import { Hooks } from "./Hooks.jsx";
+// import { Hooks } from "./Hooks.jsx";
+import { Arrays } from "./Arrays.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Hooks />);
+ReactDOM.createRoot(document.getElementById("root")).render(<Arrays />);
